fe11ed1540b9b3ddc22777d00c460b47f4e89bfe
Michelle Hettinger <Michelle Hettinger>
2016-07-15 17:07:17 -0400

    initialized trivia game. wrote functions to pick a question with a set of answers and display it to the screen. currently it gets displayed every second. a timer also counts down every second.

---
+++ assets/js/game.js
@@ -0,0 +1,189 @@
+//When the document is ready, perform this function.
+$(document).ready(function(){
+	timer.start();
+
+	$(".answer").on("click", questions);
+
+
+})
+
+var currentQuestion;
+var currentAnswer1;
+var currentAnswer2;
+var currentAnswer3;
+var currentAnswer4;
+
+
+
+var timer = {
+
+	time:3,
+	questionNumber:1,
+
+	start: function(){
+		counter = setInterval(timer.count, 1000); 			//Every second, call the count function
+	},
+
+	count: function(){
+
+		questionsAnswers.displayQuestion();						//Display the current question every second
+
+		$("#time-remaining").html(timer.time);				//Display time on the html page
+		timer.time--;										//Decrease time by 1
+
+		if (timer.time == 0){								//If time reaches zero
+			timer.time = 3;									//Set time back to 60
+			timer.questionNumber++;							//Increment the questionNumber by 1
+		}
+
+	},
+}
+
+var questionsAnswers = {
+
+	displayQuestion: function (){
+
+		//Assign currentQuestion as well as the current answers
+		if (timer.questionNumber == 1){
+
+			currentQuestion = questionsAnswers.q1;
+			currentAnswer1 = questionsAnswers.q1a1;
+			currentAnswer2 = questionsAnswers.q1a2;
+			currentAnswer3 = questionsAnswers.q1a3;
+			currentAnswer4 = questionsAnswers.q1a4;
+		}
+		else if(timer.questionNumber == 2){
+
+			currentQuestion = questionsAnswers.q2;
+			currentAnswer1 = questionsAnswers.q2a1;
+			currentAnswer2 = questionsAnswers.q2a2;
+			currentAnswer3 = questionsAnswers.q2a3;
+			currentAnswer4 = questionsAnswers.q2a4;
+		}
+		else if(timer.questionNumber == 3){
+
+			currentQuestion = questionsAnswers.q3;
+			currentAnswer1 = questionsAnswers.q3a1;
+			currentAnswer2 = questionsAnswers.q3a2;
+			currentAnswer3 = questionsAnswers.q3a3;
+			currentAnswer4 = questionsAnswers.q3a4;		
+		}
+		else if(timer.questionNumber == 4){
+
+			currentQuestion = questionsAnswers.q4;
+			currentAnswer1 = questionsAnswers.q4a1;
+			currentAnswer2 = questionsAnswers.q4a2;
+			currentAnswer3 = questionsAnswers.q4a3;
+			currentAnswer4 = questionsAnswers.q4a4;	
+		}
+		else if(timer.questionNumber == 5){
+			currentQuestion = questionsAnswers.q5;
+			currentAnswer1 = questionsAnswers.q5a1;
+			currentAnswer2 = questionsAnswers.q5a2;
+			currentAnswer3 = questionsAnswers.q5a3;
+			currentAnswer4 = questionsAnswers.q5a4;	
+		}
+		else if(timer.questionNumber == 6){
+
+			currentQuestion = questionsAnswers.q6;
+			currentAnswer1 = questionsAnswers.q6a1;
+			currentAnswer2 = questionsAnswers.q6a2;
+			currentAnswer3 = questionsAnswers.q6a3;
+			currentAnswer4 = questionsAnswers.q6a4;	
+		}
+		else if(timer.questionNumber == 7){
+
+			currentQuestion = questionsAnswers.q7;
+			currentAnswer1 = questionsAnswers.q7a1;
+			currentAnswer2 = questionsAnswers.q7a2;
+			currentAnswer3 = questionsAnswers.q7a3;
+			currentAnswer4 = questionsAnswers.q7a4;		
+		}
+		else if(timer.questionNumber == 8){
+
+			currentQuestion = questionsAnswers.q8;
+			currentAnswer1 = questionsAnswers.q8a1;
+			currentAnswer2 = questionsAnswers.q8a2;
+			currentAnswer3 = questionsAnswers.q8a3;
+			currentAnswer4 = questionsAnswers.q8a4;	
+		}
+		else if(timer.questionNumber == 9){
+
+			currentQuestion = questionsAnswers.q9;
+			currentAnswer1 = questionsAnswers.q9a1;
+			currentAnswer2 = questionsAnswers.q9a2;
+			currentAnswer3 = questionsAnswers.q9a3;
+			currentAnswer4 = questionsAnswers.q9a4;	
+		}
+		else if(timer.questionNumber == 10){
+
+			currentQuestion = questionsAnswers.q10;
+			currentAnswer1 = questionsAnswers.q10a1;
+			currentAnswer2 = questionsAnswers.q10a2;
+			currentAnswer3 = questionsAnswers.q10a3;
+			currentAnswer4 = questionsAnswers.q10a4;	
+		}
+
+		//Write the strings to html
+		$("#current-question").html(currentQuestion);
+		$("#answer1").html("A: " + currentAnswer1);
+		$("#answer2").html("B: " + currentAnswer2);
+		$("#answer3").html("C: " + currentAnswer3);
+		$("#answer4").html("D: " + currentAnswer4);	
+
+	},
+
+
+
+
+	q1:"Alaaaaaaaaaaaaaaaaaaaaah? blah? blah? blah? blah? blah? blah? blah?",
+		q1a1: "doot doot doot doot doot doot",
+		q1a2: "doot doot doot doot doot doot",
+		q1a3: "doot doot doot doot doot doot",
+		q1a4: "doot doot doot doot doot doot",
+	q2:"Bbbbbbbbbbbbah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q2a1: "boot doot doot doot doot doot",
+		q2a2: "boot doot doot doot doot doot",
+		q2a3: "bbbbot doot doot doot doot doot",
+		q2a4: "bbbt doot doot doot doot doot",	
+	q3:"ccccccccccclah blah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q3a1: "ccccccdoot doot doot doot doot",
+		q3a2: "ccccccdoot doot doot doot doot",
+		q3a3: "ccccc doot doot doot doot doot",
+		q3a4: "cccccdoot doot doot doot doot",	
+	q4:"dddddddddddddddddh blah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q4a1: "dddddddt doot doot doot doot",
+		q4a2: "dddddddt doot doot doot doot",
+		q4a3: "dddd doot doot doot doot",
+		q4a4: "dddddoot doot doot doot",	
+	q5:"eeeeeeeeeeeeeelah blah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q5a1: "eeeeeedoot doot doot doot doot",
+		q5a2: "eeeee doot doot doot doot doot",
+		q5a3: "eeee doot doot doot doot doot",
+		q5a4: "eeee doot doot doot doot doot",	
+	q6:"ffffffffffffh blah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q6a1: "fffff doot doot doot doot doot",
+		q6a2: "doffffot doot doot doot doot doot",
+		q6a3: "ffff doot doot doot doot doot",
+		q6a4: "fffff doot doot doot doot doot",	
+	q7:"gggggggggggglah blah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q7a1: "gggg doot doot doot doot doot",
+		q7a2: "gggg doot doot doot doot doot",
+		q7a3: "gggg doot doot doot doot doot",
+		q7a4: "gggg doot doot doot doot doot",	
+	q8:"hhhhhhhhhhhhhh blah blah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q8a1: "hhhhhh doot doot doot doot doot",
+		q8a2: "hhhhhh doot doot doot doot doot",
+		q8a3: "hhhhhh doot doot doot doot doot",
+		q8a4: "hhhhh doot doot doot doot doot",	
+	q9:"iiiiiii blah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q9a1: "iiiiii doot doot doot doot doot",
+		q9a2: "iiiiii doot doot doot doot doot",
+		q9a3: "iiiii doot doot doot doot doot",
+		q9a4: "iiiii doot doot doot doot doot",		
+	q10:"jjjjjjjjjjjjjjjjblah blah? blah? blah? blah? blah? blah? blah? blah? blah?",
+		q10a1: "jjjjj doot doot doot doot doot",
+		q10a2: "jjjjj doot doot doot doot doot",
+		q10a3: "jjjjj doot doot doot doot doot",
+		q10a4: "jjjjj doot doot doot doot doot",	
+}
