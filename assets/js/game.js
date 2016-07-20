fe7b65192c3a632116bf0abbced9f1c186744c0b
Michelle Hettinger <Michelle Hettinger>
2016-07-20 18:23:35 -0400

    typogrophy, background image, gifs after win/losing, button highlights, spacing

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,7 +65,7 @@ var timer = {
 
 		updateDisplay.questionAnswers();											//Display the questions and answers
 
-		$("#time-remaining").html("Time remaining: " + timer.time);				//Display time on the html page
+		$("#time-remaining").html("Time remaining: " + "<strong>" + timer.time + "</strong>");				//Display time on the html page
 		timer.time--;															//Decrease time by 1
 
 		if (timer.time == 0){													//If time reaches zero
@@ -76,7 +76,7 @@ var timer = {
 	},
 
 	betweenRound: function(){													//Need this function to delay time between rounds
-		timer.time = 2;															//Change time to just a few seconds
+		timer.time = 3;															//Change time to just a few seconds
 		counter2 = setInterval(timer.betweenCount, 1000); 						//Every second, call the count function
 
 	},
@@ -141,7 +141,7 @@ var updateDisplay = {
 	outOfTime: function(){
 		updateDisplay.convertCorrectAnswerToText(correctAnswer);								//Convert correctAnswer (a numerical value) to the string containing the right answer
 
-		$("#time-remaining").html("Time remaining: " + timer.time);								//Display time on the html page
+		$("#time-remaining").html("Time remaining: " + "<strong>" + timer.time + "</strong>");								//Display time on the html page
 		$("#time-remaining").append("<p>Out of time!</p>");										//Write a paragraph to the page that says out of time
 
 		$("#current-question").html("The correct answer was: " + correctAnswer);				//Update with the correct answer as well
@@ -176,7 +176,7 @@ var questionsAnswers = {
 	checkGuess: function(guessedLetter){
 		clearInterval(counter);													//Stop the counter
 
-		$("#time-remaining").html("Time remaining: " + timer.time);				//Display time on the html page
+		$("#time-remaining").html("Time remaining: " + "<strong>" + timer.time + "</strong>");				//Display time on the html page
 
 		if (guessedLetter == correctAnswer){									//If the selected answer matches the correct
 			updateDisplay.correctGuess();										//Display
@@ -198,8 +198,8 @@ var questionsAnswers = {
 			currentAnswer3 = questionsAndAnswers[questionNumber - 1].answers[2];
 			currentAnswer4 = questionsAndAnswers[questionNumber - 1].answers[3];
 			correctAnswer = questionsAndAnswers[questionNumber - 1].correctAnswer;
-			loseImageHTML = questionsAndAnswers.loseImage;
-			winImageHTML = questionsAndAnswers.winImage;			
+			loseImageHTML = questionsAndAnswers[questionNumber - 1].loseImage;
+			winImageHTML = questionsAndAnswers[questionNumber - 1].winImage;			
 		},
 
 }
@@ -210,70 +210,70 @@ var questionsAndAnswers =
 	question: "What city did Jazz emerge from in the early 1900's?",
 	answers: ["New Orleans","Memphis","Chicago","New York"],
 	correctAnswer: 1,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1W.png' />",
+	loseImage: "<img src='assets/images/q1L.gif' />",
+	winImage: "<img src='assets/images/q1W.gif' />",
 	},
 	{
 	question: "What was the name of the most famous jazz club in Harlem?",
 	answers: ["The Cotton Club", "Studio 54", "El Morocco", "Cotton Club"],
 	correctAnswer: 3,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",
+	loseImage: "<img src='assets/images/q2L.gif' />",
+	winImage: "<img src='assets/images/q2W.gif' />",
 	},
 	{
-	question: "At the turn of the 20th century, the term *jazz* was used interchangably with:",
+	question: "At the turn of the 20th century, the term 'jazz' was used interchangably with:",
 	answers: ["Swing", "Blues", "Neo Blues", "Rag Time"],
 	correctAnswer: 4,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",	
+	loseImage: "<img src='assets/images/q3L.gif' />",
+	winImage: "<img src='assets/images/q3W.gif' />",	
 	},
 	{
 	question: "What group or artist recorded the first official jazz record?",
-	answers: ["Duke Ellington's Orchestra", "The Dixiland Jazz Band", "Jelly Roll Morton", "Joseph King Oliver"],
+	answers: ["Duke Ellington's Orchestra", "The Dixieland Jazz Band", "Jelly Roll Morton", "Joseph King Oliver"],
 	correctAnswer: 2,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",
+	loseImage: "<img src='assets/images/q4L.gif' />",
+	winImage: "<img src='assets/images/q4W.gif' />",
 	},
 	{
 	question: "Who was known as *the Divine One*, who won an amateur singing contest as a teenager and became a leading jazz vocalist?",
 	answers: ["Sarah Vaughan", "Louis Armstrong", "Ella Fitzgerald", "Billie Holiday"],
 	correctAnswer: 1,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",
+	loseImage: "<img src='assets/images/q5L.gif' />",
+	winImage: "<img src='assets/images/q5W.gif' />",
 	},
 	{
-	question: "In the 1930's, Benny Goodman helped popularize what type of jazz?",
+	question: "In the 1930's, clarinetist Benny Goodman helped popularize what type of jazz?",
 	answers: ["Big Band", "Be-Bop", "Swing", "Afro-Latin"],
 	correctAnswer: 3,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",
+	loseImage: "<img src='assets/images/q6L.gif' />",
+	winImage: "<img src='assets/images/q6W.gif' />",
 	},
 	{
 	question: "What musician did NOT record on the 1959 hit record, *Kind of Blue*?",
 	answers: ["Miles Davis", "John Coltrane", "Bill Evans", "Clifford Brown"],
 	correctAnswer: 4,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",
+	loseImage: "<img src='assets/images/q7L.gif' />",
+	winImage: "<img src='assets/images/q7W.gif' />",
 	},
 	{
 	question: "Which of the following is NOT a Miles Davis album name?",
 	answers: ["Bitches Brew", "Blue in Green", "TUTU", "Birth of the Cool"],
 	correctAnswer: 2,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",
+	loseImage: "<img src='assets/images/q8L.gif' />",
+	winImage: "<img src='assets/images/q8W.gif' />",
 	},
 	{
 	question: "Which of the following is NOT a jazz musician?",
 	answers: ["Cannonball Adderly", "John McLaughlin", "Kenny G", "Kenny Garrett"],
 	correctAnswer: 3,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",
+	loseImage: "<img src='assets/images/q9L.gif' />",
+	winImage: "<img src='assets/images/q9W.gif' />",
 	},
 	{
 	question: "Which jazz musician is responsible for the development of *world music*, which blended jazz with non-western cultural musical styles?",
 	answers: ["John McLaughlin", "John Coltrane", "Allan Holdsworth", "Miles Davis"],
 	correctAnswer: 1,
-	loseImage: "<img src='assets/images/q1L.png' />",
-	winImage: "<img src='assets/images/q1L.png' />",
+	loseImage: "<img src='assets/images/q10L.gif' />",
+	winImage: "<img src='assets/images/q10W.jpg' />",
 	},
 ]
